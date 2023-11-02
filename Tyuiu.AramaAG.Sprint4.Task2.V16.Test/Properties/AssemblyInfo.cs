ed50206080c829a741e2cd719076d6ff229e7dcf
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-11-02 18:40:55 +0500

    Task2

---
+++ Tyuiu.AramaAG.Sprint4.Task2.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AramaAG.Sprint4.Task2.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AramaAG.Sprint4.Task2.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8d8b1d8-7afb-4af8-bac4-7c0335f36dd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
