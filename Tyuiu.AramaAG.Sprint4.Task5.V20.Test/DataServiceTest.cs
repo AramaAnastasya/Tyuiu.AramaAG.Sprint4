cd29d744d65b059cbf4a536fc51355c43ce84127
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-11-09 13:04:34 +0500

    Task5

---
+++ Tyuiu.AramaAG.Sprint4.Task5.V20.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AramaAG.Sprint4.Task5.V20.Lib;
+
+namespace Tyuiu.AramaAG.Sprint4.Task5.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[,] numsArray = new int[5, 5] { { -1, -7, 2, -6, 3 }, { -4, 4, -4, -2, -4 }, { -5, 5, -5, -6, -5 }, { -4, -2, -5, -2, 1 }, { -6, -2, 3, -6, -7 } };
+            int res = ds.Calculate(numsArray);
+            int wait = 18;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AramaAG.Sprint4.Task5.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AramaAG.Sprint4.Task5.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AramaAG.Sprint4.Task5.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7564878e-899c-4c55-aacb-30ddaaa61acd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
